f9c0da83ed384130b4441a2081d51c6556b16975
Lassi Köykkä <47752198+Lassi-Koykka@users.noreply.github.com>
2020-06-06 19:16:30 +0300

    Some UI changes

--- src/Components/fadeInSection.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-function fadeInProjects(props) {
-  const [isVisible, setVisible] = React.useState(false);
-  const domRef = React.useRef();
-}
