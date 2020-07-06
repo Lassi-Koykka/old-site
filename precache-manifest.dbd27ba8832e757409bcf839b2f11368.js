fbb4b9dd431bede79fbae06d2b185ed551bd9acd
Erik Dubois <lassi_koykka@hotmail.com>
2020-07-06 12:10:32 +0300

    Updates

---
+++ precache-manifest.dbd27ba8832e757409bcf839b2f11368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7957b894fc31dfcfcee99ff1497c00fe",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "ef8879887182f138dbec",
+    "url": "/portofolio/static/css/main.08cec3b8.chunk.css"
+  },
+  {
+    "revision": "3cb853a1edf7493241e0",
+    "url": "/portofolio/static/js/2.675d6511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portofolio/static/js/2.675d6511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8879887182f138dbec",
+    "url": "/portofolio/static/js/main.2be51326.chunk.js"
+  },
+  {
+    "revision": "216722b216134d1032b9",
+    "url": "/portofolio/static/js/runtime-main.ab862f7e.js"
+  }
+]);
